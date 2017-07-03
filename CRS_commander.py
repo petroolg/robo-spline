3a473c38092080dc6d75b1ae5a8e976b34d659c4
Olga <olga.petrova@cvut.cz>
2017-07-03 17:55:52 +0200

    Create project.

---
+++ CRS_commander.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+''' Module provides functions for robot control via Mars 8 unit. '''
+
+import sys
+import serial
+import time
+import numpy as np
+import argparse
+
+__author__ = "Pavel Pisa, Olga Petrova"
+__status__ = "Development"
+__date__ = "03.07.2017"
+
+class Robot:
+
+    def __init__(self, params):
+        pass
+
+        
