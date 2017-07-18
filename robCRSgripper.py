5c8e83ba97abebfe23e874aff9a74c3edd8b7ade
Olga <olga.petrova@cvut.cz>
2017-07-18 18:49:21 +0200

    Update robot and gripper structures.

--- robCRSgripper.py
+++ robCRSgripper.py
@@ -18,39 +18,40 @@ def robCRSgripper(self, commander, power):
       pos = ((1 - power) * b(1) + (power - 0.1) * f(1)) / 0.9
 
     # Release errors and reset controller
-     commander.rcon.write('RELEASE%s:' % commander.robot.gripper_ax)
+     commander.rcon.write('RELEASE%s:\n' % commander.robot.gripper_ax)
 
     # Set position on gripper
-     commander.rcon.write('G%s:%d' % (commander.robot.gripper_ax, pos))
+     commander.rcon.write('G%s:%d\n' % (commander.robot.gripper_ax, pos))
 
     # Release motor of open gripper
     if (power == 0):
      commander.wait_gripper_ready() #, pos )
-     commander.rcon.write('RELEASE%s:' % commander.robot.gripper_ax)
+     commander.rcon.write('RELEASE%s:\n' % commander.robot.gripper_ax)
 
 
 
 def robCRSgripperinit(controller):
     m = controller.robot.gripper_ax
     # Set  analog  mode  of controller
-    controller.rcon.write('ANAXSETUP%s:%i,%i' %(m, controller.robot.gripper_ADC, controller.robot.gripper_current))
+    controller.rcon.write('ANAXSETUP%s:%i,%i\n' %(m, controller.robot.gripper_ADC, controller.robot.gripper_current))
     
     # Maximal  curent  limit(0 - 255)
-    controller.rcon.write('REGS1%s:%i' % (m, controller.robot.gripper_current))
+    controller.rcon.write('REGS1%s:%i\n' % (m, controller.robot.gripper_current))
     
     # Limitation  constant(feedback  from overcurrent)
-    controller.rcon.write('REGS2%s:%i' % (m, controller.robot.gripper_feedback))
+    controller.rcon.write('REGS2%s:%i\n' % (m, controller.robot.gripper_feedback))
     
     # Maximal  energy  limits  voltage  on  motor
-    controller.rcon.write('REGME%s:%i' % (m, controller.robot.gripper_REGME))
+    controller.rcon.write('REGME%s:%i\n' % (m, controller.robot.gripper_REGME))
     
     # Maximal  speed
-    controller.rcon.write('REGMS%s:%i' % (m, controller.robot.gripper_REGMS))
+    controller.rcon.write('REGMS%s:%i\n' % (m, controller.robot.gripper_REGMS))
     
     # Axis  configuration  word
-    controller.rcon.write('REGCFG%s:%i' % (m, controller.robot.gripper_REGCFG))
+    controller.rcon.write('REGCFG%s:%i\n' % (m, controller.robot.gripper_REGCFG))
     
     # PID  parameters  of  controller
-    controller.rcon.write('REGP%s:%i' % (m, controller.robot.gripper_REGP))
-    controller.rcon.write('REGI%s:%i' % (m, controller.robot.gripper_REGI))
-    controller.rcon.write('REGD%s:%i' % (m, controller.robot.gripper_REGD))
+    controller.rcon.write('REGP%s:%i\n' % (m, controller.robot.gripper_REGP))
+    controller.rcon.write('REGI%s:%i\n' % (m, controller.robot.gripper_REGI))
+    controller.rcon.write('REGD%s:%i\n' % (m, controller.robot.gripper_REGD))
+    
--- robotCRS97.py
+++ robotCRS97.py
@@ -7,7 +7,160 @@ from robCRSgripper import robCRSgripper, robCRSgripperinit
 #  based on BlueBot and Bosch Toolbox
 #  by  O. Certik, V. Smutny, P. Krsek, M. Matousek
 
-class Robot:
+class robCRS:
+    """Common specifications for CRS robots"""
+    def __init__(self):
+        """Common specification for CRS robots."""
+
+        self.portname = 'COM4'
+        self.controller = 'MARS8'
+        self.demoname = 'robCRSdemo'
+
+        self.DOF = 6
+
+        # Robot link dimensions (these constants are read from documentation)
+        inch2mm = 25.4  # conversion constant
+        self.L1 = 13 * inch2mm - 25  # J2 above working desk
+        self.L2 = 12 * inch2mm  # J2-J3
+        self.L3 = 13 * inch2mm  # J3-J5
+        self.L4 = 3 * inch2mm  # J5-flange
+        self.L5 = inch2mm * (3.78 + 0.5)  # gripper length
+        self.L6 = inch2mm * 1  # gripper finger length
+
+        # Posible change of coordinate direction
+        self.direction = np.array([1, 1, -1, -1, -1, -1])
+
+        # IRC per rotation of motor.
+        self.irc = np.array([1000, 1000, 1000, 500, 500, 500])  # from manual, it should work
+        # motors' gearing
+        self.gearing = np.array([100, 100, 100, 101, 100, 101])  # from manual
+        # degtoirc=1 degree in IRC
+        self.degtoirc = self.irc * self.direction * self.gearing * 4 / 360
+
+        self.activemotors = 'ABCDEF'
+        self.hh_axes_list = 'ABCDEF'
+        self.control_axes_list = 'ABCDEF'
+        self.coord_axes = 'ABCDEF'
+
+        # DH notation
+        self.offset = np.array([0.0, 270.0, 90.0, 0.0, 0.0, 0.0]) / 180 * np.pi
+        self.sign = np.array([1, 1, 1, 1, 1, 1])
+        self.d = [self.L1, 0, 0, self.L3, 0, self.L4 + self.L5 + self.L6]
+        self.a = [0, self.L2, 0, 0, 0, 0]
+        self.alpha = np.array([90.0, 0.0, 270.0, 90.0, 270.0, 0]) / 180 * np.pi
+        self.base = np.eye(4)
+        self.tool = np.array([[0, 0, -1, 0],
+                              [0, 1, 0, 0],
+                              [1, 0, 0, 0],
+                              [0, 0, 0, 1]])
+
+        # Base position in degrees
+        # (corresponds to position robot.hhirc in IRC)
+        self.hhdeg = [0, 0, 0, 0, 0, 0]
+
+        # Softhome position in degrees
+        #  - Robot is moved to this position during startup and closing.
+        #  - The position is set by calling bbsofthome
+        self.shdeg = [0, 0, -45, 0, -45, 0]
+
+        self.REGME = [32000, 32000, 32000, 32000, 32000, 32000]
+
+        # Constants of PID regulators
+        self.REGP = [10, 12, 70, 35, 45, 100]
+        self.REGI = [80, 63, 50, 80, 65, 300]
+        self.REGD = [300, 200, 200, 130, 230, 350]
+
+        # min and max speeds (IRC/256/msec)
+        self.minspeed = np.rint(self.defaultspeed / 5)
+        self.maxspeed = np.rint(self.defaultspeed * 2)
+
+        # min a max acceleration
+        self.minacceleration = np.rint(self.defaultacceleration / 5)
+        self.maxacceleration = np.rint(self.defaultacceleration * 2)
+
+        # smutneho hodnoty robot.REGCFG = [1370 1369 1369 1361 1369 1369]
+        # D ma HW problem, protoze indexova znacka se casto nachazi o
+        # (mnoho)otacek jinde
+        # E nema indexy nikdy,
+        # problem D a E identifikovan: V rozporu s dokumentaci ma HEDS na D a E
+        # signal znacky v podmnozine kombinace kanalu AB=00, ackoliv by mel jit az
+        # do hrany A mezi 0 a 1 (ci naopak), pricemz B ma byt nula. Cip, ktery
+        # zpracovava signaly to jinak neumi.
+        # Potencialne to lze resit nejakym RC clankem mezi HEDS a budicem v robotu,
+        # daji se ocekavat problemy s ruznymi rychlostmi. Od tohoto reseni je
+        # upusteno.
+        # V teto verzi tedy D a E najizdi jen na MARK, nikoliv na indexovou znacku.
+        # robot.REGCFG = [1494 1494 1494 1484 1476 1492]# D melo 1498,
+        # robot.REGCFG = [1492 1494 1494 1484 1476 1492]# D melo 1498,
+
+        # Uprava rychlosti HOME
+        # robot.REGCFG = [1490 1491 1491 1482 1474 1490]# D melo 1498,
+
+        # Speed reduced due to power suply problem
+        self.REGCFG = [1489, 1490, 1490, 1481, 1474, 1490]  # D melo 1498,
+
+        # CRS robot shall not release after some time
+        self.IDLEREL = 1200
+
+        # homing sequence TODO
+        # robot.hardhome = robCRShardhome
+
+
+        self.timeout = 200
+
+        # Gripper parameters
+
+        self.gripper = robCRSgripper
+        self.gripper_init = robCRSgripperinit
+
+
+        # Kind of gripper (CRSGripper, Magnetic)
+        self.gripper_type = 'CRSGripper'
+
+        # Analog input of position sensor
+        self.gripper_ADC = 10
+        # Maximal curent limit (0-255)
+        # Value 16 corresponds to 250mA when feedback is 500
+        self.gripper_current = 16
+        # Limitation constant (feeadback from overcurrent)
+        self.gripper_feedback = 500
+        # Maximal energy limits voltage on motor
+        # (0 - 32000 corresponds to 0-24V)
+        # 2010-02-17 Martin Matousek note:
+        # Manual says that gripper can survive up to 15 Volts, but
+        # force more than 75# must not be used longer then 15 second. Thus
+        # safe power seems to be 11.25 V. We are using more conservative value here
+        # 10/32 * 24 = 7.5 V
+
+        self.gripper_REGME = 10000
+        # Maximal speed
+        self.gripper_REGMS = 500
+        # Axis configuration word
+        self.gripper_REGCFG = 256
+        # PID parameter of controller
+        self.gripper_REGP = 200
+        self.gripper_REGI = 0
+        self.gripper_REGD = 100
+
+        self.gripper_poll_time = 0.2
+        self.gripper_poll_diff = 50
+
+        self.ikt = robCRSikt
+
+        self.degtoirc = np.multiply(self.direction, np.multiply(self.irc, self.gearing)) * 4.0 / 360.0
+
+        self.anglestoirc = lambda angles: np.multiply((angles - self.hhdeg), self.degtoirc) + self.hhirc
+        self.irctoangles = lambda irc: np.divide(irc - self.hhirc, self.degtoirc) + self.hhdeg
+
+        degtorad = lambda d: d * np.pi / 180.0
+        radtodeg = lambda d: d * 180.0 / np.pi
+
+    # if __name__ == '__main__':
+    #     robot = Robot()
+    #     print robot.ikt(robot, [600, -200.0, 500.0, 0.0, 0.0, 0.0])
+
+
+class robCRS97(robCRS, object):
     """Robot specification: CRS97"""
 
     def __init__(self):
@@ -35,155 +188,5 @@ class Robot:
         self.verbose = True
         # ------------------------------------------------------------------------------
         # Common parameters for CRS selfs
-        self = robCRScommon(self)
+        super(robCRS97, self).__init__()
 
-
-def robCRScommon(robot):
-    """Common specification for CRS robots."""
-
-    robot.portname = 'COM4'
-    robot.controller = 'MARS8'
-    robot.demoname = 'robCRSdemo'
-
-    robot.DOF = 6
-
-    # Robot link dimensions (these constants are read from documentation)
-    inch2mm = 25.4  # conversion constant
-    robot.L1 = 13 * inch2mm - 25  # J2 above working desk
-    robot.L2 = 12 * inch2mm  # J2-J3
-    robot.L3 = 13 * inch2mm  # J3-J5
-    robot.L4 = 3 * inch2mm  # J5-flange
-    robot.L5 = inch2mm * (3.78 + 0.5)  # gripper length
-    robot.L6 = inch2mm * 1  # gripper finger length
-
-    # Posible change of coordinate direction
-    robot.direction = np.array([1, 1, -1, -1, -1, -1])
-
-    # IRC per rotation of motor.
-    robot.irc = np.array([1000, 1000, 1000, 500, 500, 500])  # from manual, it should work
-    # motors' gearing
-    robot.gearing = np.array([100, 100, 100, 101, 100, 101])  # from manual
-    # degtoirc=1 degree in IRC
-    robot.degtoirc = robot.irc * robot.direction * robot.gearing * 4 / 360
-
-    robot.activemotors = 'ABCDEF'
-    robot.hh_axes_list = 'ABCDEF'
-    robot.control_axes_list = 'ABCDEF'
-    robot.coord_axes = 'ABCDEF'
-
-    # DH notation
-    robot.offset = np.array([0.0, 270.0, 90.0, 0.0, 0.0, 0.0]) / 180 * np.pi
-    robot.sign = np.array([1, 1, 1, 1, 1, 1])
-    robot.d = [robot.L1, 0, 0, robot.L3, 0, robot.L4 + robot.L5 + robot.L6]
-    robot.a = [0, robot.L2, 0, 0, 0, 0]
-    robot.alpha = np.array([90.0, 0.0, 270.0, 90.0, 270.0, 0]) / 180 * np.pi
-    robot.base = np.eye(4)
-    robot.tool = np.array([[0, 0, -1, 0],
-                          [0, 1, 0, 0],
-                          [1, 0, 0, 0],
-                          [0, 0, 0, 1]])
-
-    # Base position in degrees
-    # (corresponds to position robot.hhirc in IRC)
-    robot.hhdeg = [0, 0, 0, 0, 0, 0]
-
-    # Softhome position in degrees
-    #  - Robot is moved to this position during startup and closing.
-    #  - The position is set by calling bbsofthome
-    robot.shdeg = [0, 0, -45, 0, -45, 0]
-
-    robot.REGME = [32000, 32000, 32000, 32000, 32000, 32000]
-
-    # Constants of PID regulators
-    robot.REGP = [10, 12, 70, 35, 45, 100]
-    robot.REGI = [80, 63, 50, 80, 65, 300]
-    robot.REGD = [300, 200, 200, 130, 230, 350]
-
-    # min and max speeds (IRC/256/msec)
-    robot.minspeed = np.rint(robot.defaultspeed / 5)
-    robot.maxspeed = np.rint(robot.defaultspeed * 2)
-
-    # min a max acceleration
-    robot.minacceleration = np.rint(robot.defaultacceleration / 5)
-    robot.maxacceleration = np.rint(robot.defaultacceleration * 2)
-
-    # smutneho hodnoty robot.REGCFG = [1370 1369 1369 1361 1369 1369]
-    # D ma HW problem, protoze indexova znacka se casto nachazi o
-    # (mnoho)otacek jinde
-    # E nema indexy nikdy,
-    # problem D a E identifikovan: V rozporu s dokumentaci ma HEDS na D a E
-    # signal znacky v podmnozine kombinace kanalu AB=00, ackoliv by mel jit az
-    # do hrany A mezi 0 a 1 (ci naopak), pricemz B ma byt nula. Cip, ktery
-    # zpracovava signaly to jinak neumi.
-    # Potencialne to lze resit nejakym RC clankem mezi HEDS a budicem v robotu,
-    # daji se ocekavat problemy s ruznymi rychlostmi. Od tohoto reseni je
-    # upusteno.
-    # V teto verzi tedy D a E najizdi jen na MARK, nikoliv na indexovou znacku.
-    # robot.REGCFG = [1494 1494 1494 1484 1476 1492]# D melo 1498,
-    # robot.REGCFG = [1492 1494 1494 1484 1476 1492]# D melo 1498,
-
-    # Uprava rychlosti HOME
-    # robot.REGCFG = [1490 1491 1491 1482 1474 1490]# D melo 1498,
-
-    # Speed reduced due to power suply problem
-    robot.REGCFG = [1489, 1490, 1490, 1481, 1474, 1490]  # D melo 1498,
-
-    # CRS robot shall not release after some time
-    robot.IDLEREL = 1200
-
-    # homing sequence TODO
-    # robot.hardhome = robCRShardhome
-
-
-    robot.timeout = 200
-
-    # Gripper parameters
-
-    robot.gripper = robCRSgripper
-    robot.gripper_init = robCRSgripperinit
-
-
-    # Kind of gripper (CRSGripper, Magnetic)
-    robot.gripper_type = 'CRSGripper'
-
-    # Analog input of position sensor
-    robot.gripper_ADC = 10
-    # Maximal curent limit (0-255)
-    # Value 16 corresponds to 250mA when feedback is 500
-    robot.gripper_current = 16
-    # Limitation constant (feeadback from overcurrent)
-    robot.gripper_feedback = 500
-    # Maximal energy limits voltage on motor
-    # (0 - 32000 corresponds to 0-24V)
-    # 2010-02-17 Martin Matousek note:
-    # Manual says that gripper can survive up to 15 Volts, but
-    # force more than 75# must not be used longer then 15 second. Thus
-    # safe power seems to be 11.25 V. We are using more conservative value here
-    # 10/32 * 24 = 7.5 V
-
-    robot.gripper_REGME = 10000
-    # Maximal speed
-    robot.gripper_REGMS = 500
-    # Axis configuration word
-    robot.gripper_REGCFG = 256
-    # PID parameter of controller
-    robot.gripper_REGP = 200
-    robot.gripper_REGI = 0
-    robot.gripper_REGD = 100
-
-    robot.gripper_poll_time = 0.2
-    robot.gripper_poll_diff = 50
-
-    robot.ikt = robCRSikt
-
-    robot.degtoirc = np.multiply(robot.direction, np.multiply(robot.irc, robot.gearing)) * 4.0 / 360.0
-
-    robot.anglestoirc = lambda angles: np.multiply((angles - robot.hhdeg), robot.degtoirc) + robot.hhirc
-    robot.irctoangles = lambda irc: np.divide(irc - robot.hhirc, robot.degtoirc) + robot.hhdeg
-
-    degtorad = lambda d: d * np.pi / 180.0
-    radtodeg = lambda d: d * 180.0 / np.pi
-
-# if __name__ == '__main__':
-#     robot = Robot()
-#     print robot.ikt(robot, [600, -200.0, 500.0, 0.0, 0.0, 0.0])
